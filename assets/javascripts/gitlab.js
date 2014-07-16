d6dcafa4b2322aad1569915784f40c69b951fd47
Sazor <sazor.mail@gmail.com>
2014-07-16 18:36:35 +0400

    Authorization flash message

--- assets/javascripts/gitlab.js
+++ assets/javascripts/gitlab.js
@@ -4,8 +4,14 @@ $(document).ready(function(){
 		var login = $('#gitlab_login').val();
 		var pass = $('#gitlab_pass').val();
 		var url = $('#gitlab_url').val() + '/api/v3';
-		$.post(url + '/session', { login: login, password: pass }, function(data){
-			gitlab = data;
-		});
+		$.post(url + '/session', { login: login, password: pass })
+			.done(function(data){
+				gitlab = data;
+				$('#auth_status').empty().append("Successful authorization!").removeClass("error notice").addClass("notice").show();
+			})
+			.fail(function(data){
+				gitlab = {};
+				$('#auth_status').empty().append("Authorization failed!").removeClass("error notice").addClass("error").show();
+			});
 	});
 });
