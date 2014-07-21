fbc2b4c935488849588385669f4876cada580752
Sazor <sazor.mail@gmail.com>
2014-07-21 19:43:41 +0400

    Gitlab in module menu

--- init.rb
+++ init.rb
@@ -10,7 +10,8 @@ Redmine::Plugin.register :gitlab_int do
   url 'https://github.com/Sazor/redmine_gitlab_integration'
   author_url 'https://github.com/Sazor'
   settings :default => {'empty' => true}, :partial => 'settings/gitlab_int_settings'
-  permission :git_lab_repositories, { :git_lab_repositories => :index }, :public => true
-  menu :project_menu, :git_lab_repositories, { :controller => 'git_lab_repositories', :action => 'index' }, :caption => 'GitLabRepository', :after => :activity, :param => :project_id
+  menu :project_menu, :git_lab_repositories, { :controller => 'git_lab_repositories', :action => 'index' }, :caption => 'GitLab', :after => :activity, :param => :project_id
+  project_module 'GitLab' do
+    permission :git_lab_repositories, :git_lab_repositories => :index
+  end
 end
-
