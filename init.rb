883e484cb191687b7f9603771cf744d11dddb3c5
Sazor <sazor.mail@gmail.com>
2014-07-21 20:26:06 +0400

    Change version (0.1.0)

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :gitlab_int do
   name 'Gitlab Integration'
   author 'Andrew Kozlov'
   description 'A plugin for close warm relations between Redmine and Gitlab.'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/Sazor/redmine_gitlab_integration'
   author_url 'https://github.com/Sazor'
   settings :default => {'empty' => true}, :partial => 'settings/gitlab_int_settings'
