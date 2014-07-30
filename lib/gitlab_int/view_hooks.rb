b4012fd0901bf38873a614df0d3a43a8f9191287
Sazor <sazor.mail@gmail.com>
2014-07-30 15:50:44 +0400

    Move gitlab token field from Edit User to My Account

--- lib/gitlab_int/view_hooks.rb
+++ lib/gitlab_int/view_hooks.rb
@@ -3,7 +3,7 @@ module GitlabInt
     class Hooks  < Redmine::Hook::ViewListener
       render_on(:view_projects_form, partial: 'git_lab_repositories/gitlab')
       render_on(:view_layouts_base_html_head, partial: 'git_lab_repositories/scripts')
-      render_on(:view_users_form, partial: 'git_lab_repositories/token_form')
+      render_on(:view_my_account, partial: 'git_lab_repositories/token_form')
     end
   end
 end
