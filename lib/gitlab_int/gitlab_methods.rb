b22fc6b8e03c999fb08e2efe34ab2e560fe6cc26
Sazor <sazor.mail@gmail.com>
2014-10-07 23:28:29 +0400

    Members synchronization now available in 3 ways(ldap, token and disabled)

--- lib/gitlab_int/gitlab_methods.rb
+++ lib/gitlab_int/gitlab_methods.rb
@@ -21,12 +21,28 @@ module GitlabInt
 			gitlab.delete_project(attrs[:id])
 		end
 
+		def gitlab_destroy_all(attrs)
+			gitlab = gitlab_configure(attrs[:token])
+			projects = gitlab.projects.map(&:to_h)
+			projects.each do |p|
+				gitlab.delete_project(p["id"])
+			end
+		end
+
+		def gitlab_get_members(options={})
+			gitlab = gitlab_configure(options[:token])
+			gitlab.team_members(options[:id])
+		end
+
 		def gitlab_member(options={})
 			login, repo_ids, role_id, op = options[:login], options[:repositories], options[:role], options[:op]
 			gitlab = gitlab_configure(options[:token])
 			# There is no searching by login, so we have to do it manually
-			all_users = gitlab.users
-			user = all_users.select { |u| u.username == login }.first
+			user =  if Setting.plugin_redmine_gitlab_integration['gitlab_members_sync'] == "ldap"
+								gitlab.users(per_page: 100, page: 0).select { |u| u.username == login }.first
+							else
+								gitlab_get_user(login) # by Gitlab token
+							end
 			#  Each repository from our list
 			repo_ids.each do |id|
 				if op == :add
@@ -43,9 +59,13 @@ module GitlabInt
 			members, repo_id = options[:members], options[:repository]
 			gitlab = gitlab_configure(options[:token])
 			# There is no searching by login, so we have to do it manually
-			all_users = gitlab.users
+			all_users = gitlab.users(per_page: 100, page: 0) if Setting.plugin_redmine_gitlab_integration['gitlab_members_sync'] == "ldap"
 			members.each do |member|
-				user = all_users.select { |u| u.to_h['username'] == member[:login] }.first
+				user =  if Setting.plugin_redmine_gitlab_integration['gitlab_members_sync'] == "ldap"
+					all_users.select { |u| u.username == login }.first
+				else
+					gitlab_get_user(login)
+				end
 				gitlab.add_team_member(repo_id, user.id, ROLES[member[:role]])
 			end
 		end
@@ -59,7 +79,18 @@ module GitlabInt
 		end
 
 		def gitlab_configure(token)
-			Gitlab.client(endpoint: (Setting.plugin_redmine_gitlab_integration['gitlab_url'] + '/api/v3'), private_token: token)
+			Gitlab.client(endpoint: get_gitlab_url, private_token: token)
+		end
+
+		def gitlab_get_user(login)
+			user = User.where(login: login).first
+			gitlab = Gitlab.client(endpoint: get_gitlab_url, private_token: user.gitlab_token)
+			gitlab.user
+		end
+
+		private
+		def get_gitlab_url
+			Setting.plugin_redmine_gitlab_integration['gitlab_url'] + '/api/v3'
 		end
 	end
 end
--- lib/gitlab_int/member_patch.rb
+++ lib/gitlab_int/member_patch.rb
@@ -13,17 +13,17 @@ module GitlabInt
 		module InstanceMethods
 			include GitlabMethods
 			def gitlab_module_enabled_and_token_exists?
-				(project.module_enabled?("GitLab") && Setting.plugin_redmine_gitlab_integration['gitlab_members_sync'] == "true" &&
+				(project.module_enabled?("GitLab") && Setting.plugin_redmine_gitlab_integration['gitlab_members_sync'] != "disabled" &&
 										  User.current.gitlab_token && !User.current.gitlab_token.empty?)
 			end
 
 			def member_in_gitlab(op)
 				repo_ids = project.git_lab_repositories.map(&:gitlab_id).compact
 				role = case op
-					    when :add  then member_roles.first.role_id
-					    when :edit then member_roles.last.role_id
-					    else nil
-					    end
+					     when :add  then member_roles.first.role_id
+					     when :edit then member_roles.last.role_id
+					     else nil
+					     end
 				gitlab_member(login: user.login, repositories: repo_ids, token: User.current.gitlab_token, role: role, op: op)
 			end
 		end
