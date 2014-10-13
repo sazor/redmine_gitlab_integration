b11725217f61471c727629641bd9e549455d7699
Sazor <sazor.mail@gmail.com>
2014-10-13 15:42:55 +0400

    Clean up

--- test/unit/git_lab_repository_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../test_helper', __FILE__)
-
-class GitLabRepositoryTest < ActiveSupport::TestCase
-include FactoryGirl::Syntax::Methods
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
