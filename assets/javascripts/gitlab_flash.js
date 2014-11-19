944e1160d585a3ca1a195121c5d9d79e9134aef5
Sazor <sazor.mail@gmail.com>
2014-11-19 19:46:25 +0400

    Empty token notification

---
+++ assets/javascripts/gitlab_flash.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('div#content').prepend("<div class = 'flash error' id = 'flash_error'>You should input gitlab token in account settings for gitlab plugin usage!</div>");
+});
--- lib/gitlab_int/view_hooks.rb
+++ lib/gitlab_int/view_hooks.rb
@@ -4,6 +4,7 @@ module RedmineGitlabIntegration
       render_on(:view_projects_form, partial: 'git_lab_repositories/gitlab')
       render_on(:view_layouts_base_html_head, partial: 'git_lab_repositories/scripts')
       render_on(:view_my_account, partial: 'git_lab_repositories/token_form')
+      render_on(:view_layouts_base_html_head, partial: 'git_lab_repositories/flash')
     end
   end
 end
